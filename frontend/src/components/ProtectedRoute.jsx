4e71d2c7855deae1169f53aa2ad87a07f0bc4730
Kaouther2314 <adjoukawthar@gmail.com>
2025-12-01 22:14:53 +0100

    Update ProtectedRoute.jsx

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -8,21 +8,22 @@ const ProtectedRoute = ({ children, role }) => {
   if (loading) {
     return <div style={{ padding: '2rem', textAlign: 'center' }}>Chargement...</div>
   }
+return children;
+  
+ // if (!isAuthenticated) {
+  //  return <Navigate to="/login" replace />
+ // }
 
-  if (!isAuthenticated) {
-    return <Navigate to="/login" replace />
-  }
+ // if (role && user?.role !== role) {
+  //  return (
+  //    <div style={{ padding: '2rem', textAlign: 'center' }}>
+   //     <h2>Accès non autorisé</h2>
+   //     <p>Vous n'avez pas les permissions nécessaires.</p>
+   //   </div>
+   // )
+ // }
 
-  if (role && user?.role !== role) {
-    return (
-      <div style={{ padding: '2rem', textAlign: 'center' }}>
-        <h2>Accès non autorisé</h2>
-        <p>Vous n'avez pas les permissions nécessaires.</p>
-      </div>
-    )
-  }
-
-  return children
+  //return children 
 }
 
 export default ProtectedRoute
