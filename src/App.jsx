62e620889489b57b1934b485e8655e547ecc6fdb
Abdelhak Boudissa <abdouboudissa15@gmail.com>
2025-12-01 01:40:19 +0100

    ch1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-/*import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from "./context/AuthContext";
+
 import Home from './pages/home/Home';
 import AdminDashboard from './pages/admin/Dashboard';
 import HospitalDashboard from './pages/hospital/Dashboard';
@@ -7,6 +9,7 @@ import DoctorDashboard from './pages/doctor/Dashboard';
 
 function App() {
   return (
+    <AuthProvider>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
@@ -16,10 +19,11 @@ function App() {
         <Route path="/doctor/dashboard" element={<DoctorDashboard />} />
       </Routes>
     </BrowserRouter>
+    </AuthProvider>
   );
 }
 
-export default App;*/
+export default App;
 
 /*
 function App() {
@@ -31,11 +35,11 @@ function App() {
   );
 }
 
-export default App;*/
+export default App;
 import Home from './pages/home/Home';
 
 function App() {
   return <Home />;
 }
 
-export default App;
+export default App;*/
